05263cda7d56236e302de09942447718fdf229f8
Arushikha0408 <69770054+Arushikha0408@users.noreply.github.com>
2020-11-23 01:45:30 +0530

    Update train.py

--- train.py
+++ train.py
@@ -17,7 +17,6 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # documentation = "https://docs.microsoft.com/en-us/python/api/azureml-core/azureml.data.dataset_factory.tabulardatasetfactory"
 dataset_from_web = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 ds = TabularDatasetFactory.from_delimited_files(path=dataset_from_web)
-x, y = clean_data(ds)
 
 run = Run.get_context()
 
@@ -48,8 +47,10 @@ def clean_data(data):
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     return x_df, y_df # missing return statement
 
+x, y = clean_data(ds)
 # TODO: Split data into train and test sets.
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=20, random_state=42)
+
 
 def main():
     # Add arguments to script
